be2ec9d61df825d71a13abbca43986c3f6d69fce
androsv <androsg93@hotmail.com>
2020-08-30 19:55:16 -0500

    Updates

---
+++ precache-manifest.51a945d887229d590ff22b39cd1bd59d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf95fded71caa8f8d765a9b921e8441",
+    "url": "/petfindertest/index.html"
+  },
+  {
+    "revision": "4e7342f621aae9a8ec43",
+    "url": "/petfindertest/static/css/main.0efa5bd7.chunk.css"
+  },
+  {
+    "revision": "01dbe73beb79af7b4b9f",
+    "url": "/petfindertest/static/js/2.efc6abac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/petfindertest/static/js/2.efc6abac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7342f621aae9a8ec43",
+    "url": "/petfindertest/static/js/main.2581bdcb.chunk.js"
+  },
+  {
+    "revision": "520dbb4ce1e21b038bd3",
+    "url": "/petfindertest/static/js/runtime-main.da8c0bad.js"
+  },
+  {
+    "revision": "d1339207f5507008d1d89f6bab823135",
+    "url": "/petfindertest/static/media/logo.d1339207.png"
+  },
+  {
+    "revision": "4f325f5595a05428bfd8422c9ab9ab4c",
+    "url": "/petfindertest/static/media/pet.4f325f55.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1466e2f0c37837b36ef9bdf5c0ff5b21.js"
+  "/petfindertest/precache-manifest.51a945d887229d590ff22b39cd1bd59d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/petfindertest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
